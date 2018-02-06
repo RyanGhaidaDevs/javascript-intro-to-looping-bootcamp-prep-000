3840cc0f1dc8c120a3b44842c041f3b5b010fb1a
Ryan Ghaida <ryaboug@gmail.com>
2018-02-06 16:14:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -5,7 +5,7 @@
 function forLoop() {
   for (let i = 0; i < 25; i++) {
 
-if (i==1)   console.log( `"I am 1 strange loop."`)
+if (i==1)   console.log( `"I am the 1 strange loop."`)
 
 else  console.log(`"I am the ${i} strange loops."`)
 
