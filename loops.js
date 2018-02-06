e18223278e3d38e12c60813b6fe5e832502a4b32
Ryan Ghaida <ryaboug@gmail.com>
2018-02-06 16:40:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,14 +1,18 @@
 
+//push is going to add element to end of array 
 
-
-var forLoop =  function forLoop()
- {
+ function forLoop(array) {
   for (let i = 0; i < 25; i++) {
 
-if (i==1)   console.log( `"I am the 1 strange loop."`)
+if (i==1) { 
 
-else  console.log(`"I am the ${i} strange loops."`)
+  console.log( `"I am the 1 strange loop."`)
+  
+} else { console.log(`"I am the ${i} strange loops."`)
 
+}
+
+return array
 
 }
 }
